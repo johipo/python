d2c81e006045ed7d6f9456c27cdf313bd4cf5e08
johipo <jvhipolito627@gmail.com>
2022-11-06 19:03:51 -0400

    segundo commit

--- python comecando/advinhacao.py
+++ python comecando/advinhacao.py
@@ -6,4 +6,11 @@ numero_secreto = 42
 
 num_user = input("Digite o seu numero: ")
 
-print("Você digitou o número ", num_user)
+print("Você digitou o número ", num_user_str)
+
+num_user = int(num_user_str)
+
+if(numero_secreto == num_user):
+    print("Você acertou!")
+else:
+    print("Você errou!")
