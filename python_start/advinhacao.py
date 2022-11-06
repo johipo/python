738e43ab3d7d6fa70b102d78f6f089e6186048f2
johipo <jvhipolito627@gmail.com>
2022-11-06 19:23:50 -0400

    Correção do numero recebido e indicação se é maior ou menor

--- python_start/advinhacao.py
+++ python_start/advinhacao.py
@@ -4,13 +4,19 @@ print("********************************")
 
 numero_secreto = 42
 
-num_user = input("Digite o seu numero: ")
+num_user_str = input("Digite o seu numero: ")
 
-print("Você digitou o número ", num_user_str)
+print("Você digitou o número ", num_user_str, end="\n\n")
 
 num_user = int(num_user_str)
 
 if(numero_secreto == num_user):
     print("Você acertou!")
 else:
+    if(num_user > numero_secreto):
+        print("Seu chute foi maior que o número secreto")
+    elif(num_user < numero_secreto):
+        print("Seu chute foi menor que o número secreto")
     print("Você errou!")
+
+print("Fim de Jogo!!!")
