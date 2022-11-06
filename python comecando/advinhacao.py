8ee5649b22e198be4a9bce8e90260cce57de0163
johipo <jvhipolito627@gmail.com>
2022-11-06 18:57:54 -0400

    first commit

---
+++ python comecando/advinhacao.py
@@ -0,0 +1,9 @@
+print("********************************")
+print("Bem vindo ao jogo de Advinhação!")
+print("********************************")
+
+numero_secreto = 42
+
+num_user = input("Digite o seu numero: ")
+
+print("Você digitou o número ", num_user)
