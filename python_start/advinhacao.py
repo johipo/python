a218eb6f5a87422b37504c9e3513edd91508f0c9
johipo <jvhipolito627@gmail.com>
2022-11-06 20:51:02 -0400

    adicionando limite

--- python_start/advinhacao.py
+++ python_start/advinhacao.py
@@ -7,16 +7,21 @@ total_tentativas = 3
 
 for rodada in range (1, total_tentativas+1):
     print("Tentativa {} de {}".format(rodada, total_tentativas))
-    num_user_str = input("Digite o seu numero: ")
+    num_user_str = input("Digite um numero entre 1 e 100: ")
     print("Você digitou o número ", num_user_str)
-
     num_user = int(num_user_str)
+
+    if(num_user < 1 or num_user > 100):
+        print("Você deve digitar um número entre 1 e 100!!!", end="\n\n")
+        continue
+
     acertou = numero_secreto == num_user
     maior = num_user > numero_secreto
     menor = num_user < numero_secreto
 
     if acertou :
         print("Parabéns, você acertou!")
+        break
     else:
         if(maior):
             print("Seu chute foi maior que o número secreto")
